804950c31ddd0a864c43f7bb1ae57e1ff4d5da52
vlad lakin <mclnmf@gmail.com>
2018-09-03 19:15:01 -0400

    Version 2.2.1 and cleanup

--- MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/view/ConditionStateVectorView.java
+++ MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/view/ConditionStateVectorView.java
@@ -6,18 +6,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
-public class MclnConditionVector extends AbstractVector {
+public class ConditionStateVectorView extends AbstractVector {
 
     private static final Color HIGHLIGHTED_BACKGROUND_COLOR = new Color(0x77FFFF);
 
-    public MclnConditionVector(boolean horizontal, VectorDataModel vectorDataModel, int nElements, int width, int height) {
+    public ConditionStateVectorView(boolean horizontal, VectorDataModel vectorDataModel, int nElements, int width, int height) {
         super(horizontal, vectorDataModel, nElements, width, height);
     }
 
     @Override
     public void updateRecalculatedState(List<String> vectorValues) {
         for (int i = 0; i < nElements; i++) {
-            JLabel label = labelVector.get(i);
+            JLabel label = vectorCells.get(i);
             String cellValue = vectorValues.get(i);
             label.setText(cellValue);
             boolean recognized = cellValue.equalsIgnoreCase("!");
