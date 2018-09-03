45f9628d4256a28241e456dbd31dda5c261aa23d
vlad lakin <mclnmf@gmail.com>
2018-09-03 18:53:05 -0400

    Version 2.2.1 and cleanup

--- MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/model/AndMatrixDataModel.java
+++ MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/model/AndMatrixDataModel.java
@@ -6,45 +6,29 @@ import java.util.Map;
 public class AndMatrixDataModel extends MatrixDataModel {
 
     private final boolean horizontalLayout;
-    private final List<AndCell> andMatrixProperties;
     private final List<Map<Integer, AndCell>> andMatrixConditions;
 
-    public AndMatrixDataModel(boolean horizontalLayout,
-                              List<AndCell> andMatrixProperties, List<Map<Integer, AndCell>> andMatrixConditions) {
+    public AndMatrixDataModel(boolean horizontalLayout, List<Map<Integer, AndCell>> andMatrixConditions) {
         this.horizontalLayout = horizontalLayout;
-        this.andMatrixProperties = andMatrixProperties;
         this.andMatrixConditions = andMatrixConditions;
     }
 
-    public String getMatrixElement(int i, int j) {
-        if(horizontalLayout) {
-            Map<Integer, AndCell> rowCells = andMatrixConditions.get(i);
-            AndCell andCell = rowCells.get(j);
-            if (andCell != null) {
-                return andCell.getExpectedState();
-            } else {
-                return "-";
-            }
-        }else{
-            Map<Integer, AndCell> rowCells = andMatrixConditions.get(j);
-            AndCell andCell = rowCells.get(i);
-            if (andCell != null) {
-                return andCell.getExpectedState();
-            } else {
-                return "-";
-            }
-        }
+    @Override
+    public String getCellValue(int i, int j) {
+        MatrixCell matrixCell = getMatrixElement(i, j);
+        return matrixCell != null ? matrixCell.getExpectedState() : "-";
     }
 
-    /**
-     * Rows are Properties
-     * Columns are conditions
-     *
-     * @param i
-     * @param j
-     * @return
-     */
-//    public String getMatrixElementFor(int i, int j) {
-//
-//    }
+    @Override
+    public MatrixCell getMatrixElement(int i, int j) {
+        AndCell andCell;
+        if (horizontalLayout) {
+            Map<Integer, AndCell> rowCells = andMatrixConditions.get(i);
+            andCell = rowCells.get(j);
+        } else {
+            Map<Integer, AndCell> rowCells = andMatrixConditions.get(j);
+            andCell = rowCells.get(i);
+        }
+        return andCell;
+    }
 }
