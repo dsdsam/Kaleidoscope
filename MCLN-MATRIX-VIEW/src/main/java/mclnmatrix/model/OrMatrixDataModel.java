70d0475bf225d61560441161a66d21e8c1e27ed1
vlad lakin <mclnmf@gmail.com>
2018-09-03 18:53:58 -0400

    Version 2.2.1 and cleanup

--- MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/model/OrMatrixDataModel.java
+++ MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/model/OrMatrixDataModel.java
@@ -3,65 +3,32 @@ package mclnmatrix.model;
 import java.util.List;
 import java.util.Map;
 
-public class OrMatrixDataModel extends MatrixDataModel{
+public class OrMatrixDataModel extends MatrixDataModel {
 
     private final boolean horizontalLayout;
-    private final List<Map<Integer, OrCell>> orMatrixProperties;
     private final List<Map<Integer, OrCell>> orMatrixConditions;
 
-    public OrMatrixDataModel(boolean horizontalLayout,
-                             List<Map<Integer, OrCell>> orMatrixProperties, List<Map<Integer, OrCell>> orMatrixConditions) {
+    public OrMatrixDataModel(boolean horizontalLayout, List<Map<Integer, OrCell>> orMatrixConditions) {
         this.horizontalLayout = horizontalLayout;
-        this.orMatrixProperties = orMatrixProperties;
         this.orMatrixConditions = orMatrixConditions;
     }
 
-    public String getMatrixElement(int i, int j) {
-        if(horizontalLayout) {
-//            if (i >= orMatrixProperties.size()) {
-//                return "~";
-//            }
+    @Override
+    public String getCellValue(int i, int j) {
+        MatrixCell matrixCell = getMatrixElement(i, j);
+        return matrixCell != null ? matrixCell.getExpectedState() : "-";
+    }
+
+    @Override
+    public MatrixCell getMatrixElement(int i, int j) {
+        OrCell orCell;
+        if (horizontalLayout) {
             Map<Integer, OrCell> rowCells = orMatrixConditions.get(i);
-            if (rowCells == null) {
-                return "+";
-            }
-            OrCell orCell = rowCells.get(j);
-            if (orCell != null) {
-                return orCell.getExpectedState();
-            } else {
-                return "-";
-            }
-        } else{
-            /**
-             * Rows are Properties
-             * Columns are conditions
-             */
-//            if (j >= orMatrixProperties.size()) {
-//                return "~";
-//            }
+            orCell = rowCells.get(j);
+        } else {
             Map<Integer, OrCell> rowCells = orMatrixConditions.get(j);
-            if (rowCells == null) {
-                return "+";
-            }
-            OrCell andCell = rowCells.get(i);
-            if (andCell != null) {
-                return andCell.getExpectedState();
-            } else {
-                return "-";
-            }
-//            return "-";
+            orCell = rowCells.get(i);
         }
-//        if(orMatrixProperties.size() >= j){
-//            return "~";
-//        }
-//        Map<Integer, OrCell> columnCells = orMatrixProperties.get(j);
-//        if(columnCells ==  null){
-//            return "+";
-//        }
-//        OrCell orCell = columnCells.get(i);
-//        if (orCell != null) {
-//            return orCell.getExpectedState();
-//        } else
-//            return "-";
+        return orCell;
     }
 }
