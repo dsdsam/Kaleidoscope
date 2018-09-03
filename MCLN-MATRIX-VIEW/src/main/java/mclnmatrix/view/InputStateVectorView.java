a3ccfbe1e1244c700a0afef044c8e3ac1f655425
vlad lakin <mclnmf@gmail.com>
2018-09-03 19:33:36 -0400

    Version 2.2.1 and cleanup

--- MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/view/InputStateVectorView.java
+++ MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/view/InputStateVectorView.java
@@ -6,18 +6,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
-public class MclnStateVector extends AbstractVector {
+public class InputStateVectorView extends AbstractVector {
 
     private static final Color HIGHLIGHTED_BACKGROUND_COLOR = new Color(0x94D352);
 
-    public MclnStateVector(boolean horizontal, VectorDataModel vectorDataModel, int nElements, int width, int height) {
+    public InputStateVectorView(boolean horizontal, VectorDataModel vectorDataModel, int nElements, int width, int height) {
         super(horizontal, vectorDataModel, nElements, width, height);
     }
 
     @Override
     public void updateRecalculatedState(List<String> vectorValues) {
         for (int i = 0; i < nElements; i++) {
-            JLabel label = labelVector.get(i);
+            JLabel label = vectorCells.get(i);
             String cellValue = vectorValues.get(i);
             cellValue = !cellValue.equalsIgnoreCase("_") ? cellValue : " ";
             label.setText(cellValue);
