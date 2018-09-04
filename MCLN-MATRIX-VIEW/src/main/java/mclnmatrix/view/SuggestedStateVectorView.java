2def2af5bf969f5ce2bf183e467b856da5acefcd
vlad lakin <mclnmf@gmail.com>
2018-09-04 09:23:08 -0400

    Version 2.2.1 and cleanup

--- MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/view/SuggestedStateVectorView.java
+++ MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/view/SuggestedStateVectorView.java
@@ -7,23 +7,24 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
-public class GeneratedVector extends AbstractVector {
+public class SuggestedStateVectorView extends AbstractVector {
 
     private static final Color HIGHLIGHTED_BACKGROUND_COLOR = Color.ORANGE;
 
-    public GeneratedVector(boolean horizontal, VectorDataModel vectorDataModel, int nElements, int width, int height) {
+    public SuggestedStateVectorView(boolean horizontal, VectorDataModel vectorDataModel, int nElements,
+                                    int width, int height) {
         super(horizontal, vectorDataModel, nElements, width, height);
     }
 
     @Override
     public void updateRecalculatedState(List<String> vectorValues) {
         for (int i = 0; i < nElements; i++) {
-            JLabel label = labelVector.get(i);
+            JLabel label = vectorCells.get(i);
             String cellValue = vectorValues.get(i);
             cellValue = !cellValue.equalsIgnoreCase("_") ? cellValue : " ";
             label.setText(cellValue);
 
-            if (  AoSUtils.isStateSymbol(cellValue)) {
+            if (AoSUtils.isStateSymbol(cellValue)) {
                 label.setBackground(HIGHLIGHTED_BACKGROUND_COLOR);
             } else {
                 label.setBackground(CELL_BACKGROUND_COLOR);
@@ -31,18 +32,4 @@ public class GeneratedVector extends AbstractVector {
         }
         repaint();
     }
-
-    /*
-       A 0 0 #
-       M N G H
-       A N G H
-
-       0 B 0 #
-       A N G H
-       A B G H
-
-       M N G H
-       A B G H
-       A B - -
-     */
 }
