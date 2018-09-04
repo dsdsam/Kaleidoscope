2245149fa6ed6e600fe8c6ffcb1e996991f027a9
vlad lakin <mclnmf@gmail.com>
2018-09-03 21:45:36 -0400

    Version 2.2.1 and cleanup

--- MCLN-MODEL/src/main/java/mcln/model/MclnSplineArc.java
+++ MCLN-MODEL/src/main/java/mcln/model/MclnSplineArc.java
@@ -16,6 +16,62 @@ public class MclnSplineArc<InpNodeType, OutNodeType> extends MclnArc {
         super(arrowTipLocationPolicy, arcUid, cSysKnots, arcMclnState, inpNode, outNode);
     }
 
+    @Override
+    public String getOneLineInfoMessage() {
+        if (inpNode == null || outNode == null) {
+            return "Arc creation is not complete.";
+        }
+        oneLineMessageBuilder.delete(0, oneLineMessageBuilder.length());
+
+
+        if (isRuntimeInitializationUpdatedFlag()) {
+            oneLineMessageBuilder.append("* ");
+        }
+
+        if (inpNode instanceof MclnStatement) {
+            oneLineMessageBuilder.append("Recognizing Arc: [");
+            oneLineMessageBuilder.append(" ID: " + getUID());
+
+            int nKnots = cSysKnots.size();
+            oneLineMessageBuilder.append(", Knots = " + nKnots);
+
+            String expectedState = (arcMclnState != null ? arcMclnState.getStateName() : "NA");
+            oneLineMessageBuilder.append(", Expected State = ");
+            oneLineMessageBuilder.append(expectedState);
+            oneLineMessageBuilder.append(" ]");
+
+            MclnState inpState = ((MclnStatement) inpNode).getCurrentMclnState();
+            MclnState calculatedProducedState = getCalculatedProducedState();
+            oneLineMessageBuilder.append("  Produced output: ");
+            oneLineMessageBuilder.append(calculatedProducedState.getStateName());
+            oneLineMessageBuilder.append("  =  P(" + expectedState);
+            oneLineMessageBuilder.append(", ");
+            oneLineMessageBuilder.append(inpState.getStateName());
+            oneLineMessageBuilder.append(");");
+        } else {
+            oneLineMessageBuilder.append("Generating Arc: [");
+            oneLineMessageBuilder.append(" ID: " + getUID());
+
+            int nKnots = cSysKnots.size();
+            oneLineMessageBuilder.append(", Knots = " + nKnots);
+
+            String generatedState = (arcMclnState != null ? arcMclnState.getStateName() : "NA");
+            oneLineMessageBuilder.append(", Generated State = ");
+            oneLineMessageBuilder.append(generatedState);
+            oneLineMessageBuilder.append(" ]");
+
+            MclnState inpState = ((MclnCondition) inpNode).getCurrentMclnState();
+            MclnState calculatedProducedState = getCalculatedProducedState();
+            oneLineMessageBuilder.append("  Produced output: ");
+            oneLineMessageBuilder.append(calculatedProducedState.getStateName());
+            oneLineMessageBuilder.append("  =  G(" + generatedState);
+            oneLineMessageBuilder.append(", ");
+            oneLineMessageBuilder.append(inpState.getStateName());
+            oneLineMessageBuilder.append(");");
+        }
+        return oneLineMessageBuilder.toString();
+    }
+
     //
     //  X M L i z e r
     //
