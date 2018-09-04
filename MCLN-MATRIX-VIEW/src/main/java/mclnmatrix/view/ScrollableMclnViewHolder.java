47e33a34333aa4a598bfdbd068c0cfc250328240
vlad lakin <mclnmf@gmail.com>
2018-09-04 09:18:44 -0400

    Version 2.2.1 and cleanup

--- MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/view/ScrollableMclnViewHolder.java
+++ MCLN-MATRIX-VIEW/src/main/java/mclnmatrix/view/ScrollableMclnViewHolder.java
@@ -1,15 +1,33 @@
 package mclnmatrix.view;
 
+import adf.ui.components.panels.ImagePanel;
+import adf.utils.BuildUtils;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class ScrollableMclnViewHolder extends JPanel {
+public class ScrollableMclnViewHolder extends ImagePanel {
+
+    private static final String IMAGES_LOCATION_CLASSPATH = "/mclnmatrix-resources/images/";
 
     private JScrollPane scrollPane;
 
     public ScrollableMclnViewHolder() {
-        super(new BorderLayout());
+        setLayout(new BorderLayout());
+        setOpaque(true);
+        setBackground(Color.BLUE);
 
+        setOpaque(false);
+        setBorder(null);
+
+        String image01 = "bg-page.jpg";
+        String image02 = "canvas.jpg";
+        String image03 = "line-bg.png";
+        String image04 = "paint-bg.jpg";
+        String image05 = "bg-3.jpg";
+        String imageClassPath = IMAGES_LOCATION_CLASSPATH + image05;
+        ImageIcon backgroundImage = BuildUtils.getImageIcon(imageClassPath);
+        setImage(backgroundImage);
     }
 
     public void initContent(JPanel panel) {
@@ -21,6 +39,9 @@ public class ScrollableMclnViewHolder extends JPanel {
         scrollPane.getViewport().setPreferredSize(panel.getPreferredSize());
         scrollPane.getViewport().setMinimumSize(panel.getMinimumSize());
         scrollPane.setBorder(null);
+        scrollPane.setOpaque(false);
+        scrollPane.getViewport().setOpaque(false);
+        panel.setOpaque(false);
         add(scrollPane, BorderLayout.CENTER);
     }
 
