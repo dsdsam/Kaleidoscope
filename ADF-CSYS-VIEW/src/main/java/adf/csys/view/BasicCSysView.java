b967b068c61970909e83b2fc27a10afc3d8379d4
vlad lakin <mclnmf@gmail.com>
2018-09-08 15:47:03 -0400

    Version 2.2.1 and cleanup

--- ADF-CSYS-VIEW/src/main/java/adf/csys/view/BasicCSysView.java
+++ ADF-CSYS-VIEW/src/main/java/adf/csys/view/BasicCSysView.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class BasicCSysView extends CSysView {
+public abstract class BasicCSysView extends CSysView {
 
     protected CSysRectangleEntity[] viewWorldEntityArray = new CSysRectangleEntity[0];
 
