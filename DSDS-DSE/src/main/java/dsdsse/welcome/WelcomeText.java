1cb9fc04f9e784b8af58861cd627a8f346d8191e
vlad lakin <mclnmf@gmail.com>
2018-09-08 16:12:41 -0400

    Version 2.2.1 and cleanup

--- DSDS-DSE/src/main/java/dsdsse/welcome/WelcomeText.java
+++ DSDS-DSE/src/main/java/dsdsse/welcome/WelcomeText.java
@@ -15,56 +15,56 @@ final class WelcomeText extends JPanel {
     private static final String WELCOME_HTML = new StringBuilder().
             append("<html>").
 //            append("<div style=\"font-size:19px; font-weight: plain; color:#780000; text-align:center; ").
-            append("<div style=\"font-size:16px; font-weight: plain; color:#AA0000; text-align:center; ").
-            append(" \">").
-            append("Welcome to Discrete Symbolic Dynamical Systems<br> ").
-            append("Development and Simulating Environment<br> ").
-            append("(DSDS) (DSE)<br> ").
-            append("</div></html>").toString();
+        append("<div style=\"font-size:14px; font-weight: plain; color:#AA0000; text-align:center; ").
+                    append(" \">").
+                    append("Welcome to Discrete Symbolic Dynamical Systems<br> ").
+                    append("Development and Simulating Environment<br> ").
+                    append("(DSDS) (DSE)<br> ").
+                    append("</div></html>").toString();
 
     private static final String INFO_HTML = new StringBuilder().
             append("<html>").
 //            append("<div style=\"font-size:14px; font-weight: plain; color:#780000; text-align:justify; ").
-            append("<div style=\" font-size:14px; font-weight: plain;    color:#550000; text-align:justify;   ").
+        append("<div style=\" font-size:12px; font-weight: plain;    color:#550000; text-align:justify;   ").
 //            append("<div  style=\" font-family:family:calibrib; font-size:13px; font-weight: plain; color:#550000; text-align:justify;  ").
 //            append("<div style=\"font-family:family:Roboto-Italic; font-size:13px;    color:#550000; text-align:justify; \"  ").
 //            append("<div style=\"font-size:13px; font-weight: plain; color:#333344; text-align:justify; ").
-            append(" \">").
-            append("The DSDS DSE is a tool to build Discrete Symbolic Dynamical Systems").
-            append(" graphical models and simulate their behavior over time.").
-            append(" Hence, the Environment may be set to work in either Development or Simulation mode.").
-            append(" Please use \"Development\" or \"Simulation\" menu to toggle between the modes.<br>").
-            append("<br>").
-            append(" When started, the Environment is in Development mode.").
-            append(" The Multicolored Logical Net (McLN) modeling formalism is used to represent the model.").
-            append(" \"Project\" menu has general operations, like \"Save As\", \"Retrieve\" and on.").
-            append(" \"Examples\" menu suggests operations to generate several predefined McLN models.").
-            append(" These models can be edited or executed.").
-            append(" The groups of menu items under \"Creation\" and \"Modification\"" +
-                    " menus are the key DSDS model creation operations.").
-            append("<br><br>").
-            append("The Simulation mode has four buttons on the toolbar.").
-            append(" \"Start\" and \"Stop\" buttons start and stop simulation process respectively.").
-            append(" \"Step\" button is used to execute one step of the simulation at a time.").
-            append(" \"Reset\" button resets the model to its initial state.").
-            append("<br><br>").
-            append("Please use \"Preferences\" menu to open DSE Preferences Setup panel and choose").
-            append(" available configuration options.").
+        append(" \">").
+                    append("The DSDS DSE is a tool to build Discrete Symbolic Dynamical Systems").
+                    append(" graphical models and simulate their behavior over time.").
+                    append(" Hence, the Environment may be set to work in either Development or Simulation mode.").
+                    append(" Please use \"Development\" or \"Simulation\" menu to toggle between the modes.<br>").
+                    append("<br>").
+                    append(" When started, the Environment is in Development mode.").
+                    append(" The Multicolored Logical Net (McLN) modeling formalism is used to represent the model.").
+                    append(" \"Project\" menu has general operations, like \"Save As\", \"Retrieve\" and on.").
+                    append(" \"Examples\" menu suggests operations to generate several predefined McLN models.").
+                    append(" These models can be edited or executed.").
+                    append(" The groups of menu items under \"Creation\" and \"Modification\"" +
+                            " menus are the key DSDS model creation operations.").
+                    append("<br><br>").
+                    append("The Simulation mode has four buttons on the toolbar.").
+                    append(" \"Start\" and \"Stop\" buttons start and stop simulation process respectively.").
+                    append(" \"Step\" button is used to execute one step of the simulation at a time.").
+                    append(" \"Reset\" button resets the model to its initial state.").
+                    append("<br><br>").
+                    append("Please use \"Preferences\" menu to open DSE Preferences Setup panel and choose").
+                    append(" available configuration options.").
 //            append("<br>").
-            append(" Follow \"Help\" menu for more detailed DSE descriptions and instructions.").
-            append("</div></html>").toString();
+        append(" Follow \"Help\" menu for more detailed DSE descriptions and instructions.").
+                    append("</div></html>").toString();
 
     private static final String TURN_OFF_HTML = new StringBuilder().
             append("<html>").
 //            append("<div style=\"font-size:10px; font-weight: plain; color:#787878; text-align:justify; ").
-            append("<div style=\"font-size:12px; font-weight: plain; color:#000050; text-align:justify; ").
-            append(" \">").
-            append("The option to turn this popup off is under \"Preferences\" menu.").
-            append("</div></html>").toString();
+        append("<div style=\"font-size:11px; font-weight: plain; color:#000050; text-align:justify; ").
+                    append(" \">").
+                    append("The option to turn this popup off is under \"Preferences\" menu.").
+                    append("</div></html>").toString();
 
     private JLabel welcomeLabel = new JLabel(WELCOME_HTML, JLabel.CENTER);
     private JLabel infoLabel = new JLabel(INFO_HTML, JLabel.CENTER);
-//    private JTextArea infoLabel = new JTextArea(INFO_HTML);
+    //    private JTextArea infoLabel = new JTextArea(INFO_HTML);
     private JLabel turnOffLabel = new JLabel(TURN_OFF_HTML, JLabel.RIGHT);
 /*
     // You should execute this part on the Event Dispatch Thread
@@ -80,6 +80,7 @@ final class WelcomeText extends JPanel {
         webView.getEngine().load("http://www.stackoverflow.com/");
     });
 */
+
     /**
      *
      */
@@ -95,7 +96,7 @@ final class WelcomeText extends JPanel {
         fixedSizePanel.setMaximumSize(TEXT_SIZE);
 
         fixedSizePanel.add(welcomeLabel, new GridBagConstraints(0, 0, 1, 1, 1.0, 0.0,
-                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 0, 0), 0, 0));
 
         fixedSizePanel.add(infoLabel, new GridBagConstraints(0, 1, 1, 1, 1.0, 0.0,
                 GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(20, 0, 0, 0), 0, 0));
